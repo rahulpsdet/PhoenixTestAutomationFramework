6a7c8854f9a7be1d2083f9540b66304088fa2782
rahulpsdet <rahulprajapati9532@gmail.com>
2026-04-25 13:27:02 +0530

    Created the JSON Reader Utility for reading the loginAPI Test data

---
+++ src/test/java/com/api/utils/JsonReaderUtil.java
@@ -0,0 +1,34 @@
+package com.api.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import com.api.request.model.UserCredentials;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonReaderUtil {
+
+	public static <T> Iterator<T> loadJSON(String fileName, Class<T[]> clazz) {
+		List<T> list = null;
+		T[] classArray;
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		try {
+			classArray = objectMapper.readValue(is, clazz);
+			list = Arrays.asList(classArray);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return list.iterator();
+
+	}
+}
