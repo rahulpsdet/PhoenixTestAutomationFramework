0a57857c94388e8965b7fcb698bac521900e19c7
rahulpsdet <rahulprajapati9532@gmail.com>
2026-04-28 00:33:56 +0530

    Created Login API Excel Data Provider
    1. Created the loadTestData(): Reads the excelfile and creates the list
    of UserCredetntials
    
    2. Updated the DataProvideerUtile: Cretaed new Dataprovider called:
    LoginAPIExcelDataProvider
    
    3. Introduced new test for LoginAPIExcelDataDrivenTest

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -10,6 +10,7 @@ import com.api.request.model.CreateJobPayload;
 import com.api.request.model.UserCredentials;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
+import com.api.utils.ExcelReaderUtil2;
 import com.api.utils.FakerDataGenerator;
 import com.api.utils.JsonReaderUtil;
 import com.dataproviders.api.bean.CreateJobBean;
@@ -66,5 +67,12 @@ public class DataProviderUtils {
 		
 		return JsonReaderUtil.loadJSON("testData/CreateJobAPIData.json", CreateJobPayload[].class);
 
+	}
+	
+	@DataProvider(name = "LoginAPIExcelDataProvider", parallel = true)
+	public static Iterator<UserCredentials> LoginAPIExcelDataProvider() {
+
+		
+		return ExcelReaderUtil2.loadExcelTestData();
 	}
 }
