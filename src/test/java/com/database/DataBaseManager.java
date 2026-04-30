e74ade21ac56f44bc548949fa594f5e603b167e3
rahulpsdet <rahulprajapati9532@gmail.com>
2026-05-01 02:00:09 +0530

    Updated the DataBaseManager to use HikaryCP Library

--- src/test/java/com/database/DataBaseManager.java
+++ src/test/java/com/database/DataBaseManager.java
@@ -1,40 +1,71 @@
 package com.database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.api.utils.ConfigManager;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DataBaseManager {
+
 	private static final String DB_URL = ConfigManager.getProperty("DB_URL");
 	private static final String DB_USERNAME = ConfigManager.getProperty("DB_USERNAME");
 	private static final String DB_PASSWORD = ConfigManager.getProperty("DB_PASSWORD");
-	private volatile static Connection conn;
+	private static final int MAXIMUM_POOL_SIZE = Integer.parseInt(ConfigManager.getProperty("MAXIMUM_POOL_SIZE"));
+	private static final int MINIMUM_IDLE_COUNT = Integer.parseInt(ConfigManager.getProperty("MINIMUM_IDLE_COUNT"));
+	private static final int CONNECTION_TIMEOUT_IN_SECS = Integer
+			.parseInt(ConfigManager.getProperty("CONNECTION_TIMEOUT_IN_SECS"));
+	private static final int IDLE_TIMEOUT_SECS = Integer.parseInt(ConfigManager.getProperty("IDLE_TIMEOUT_SECS"));
+	private static final int MAX_LIFE_TIME_IN_MINS = Integer
+			.parseInt(ConfigManager.getProperty("MAX_LIFE_TIME_IN_MINS"));
+	private static final String HIKARI_CP_POOL_NAME = ConfigManager.getProperty("HIKARI_CP_POOL_NAME");
+	private static HikariConfig hikariConfig;
+	private volatile static HikariDataSource hikariDataSource;
 
+	private static Connection conn; // Any update that happens to this conn variable!
+	// all threads will be aware of it!!!
 
 	private DataBaseManager() {
-		
+
 	}
-	public static void createConnection() throws SQLException {
-		if (conn == null) {
+
+	private static void intializePool() {
+
+		if (hikariDataSource == null) { // First Check which all the parallel threads will enter
 			synchronized (DataBaseManager.class) {
-
-				if (conn == null) {
-					conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-					System.out.println(conn);
+				if (hikariDataSource == null) { // ONLY and only for the first Connection Request
+					HikariConfig hikariConfig = new HikariConfig();
+					hikariConfig.setJdbcUrl(DB_URL);
+					hikariConfig.setUsername(DB_USERNAME);
+					hikariConfig.setPassword(DB_PASSWORD);
+					hikariConfig.setMaximumPoolSize(MAXIMUM_POOL_SIZE);
+					hikariConfig.setMinimumIdle(MINIMUM_IDLE_COUNT);
+					hikariConfig.setConnectionTimeout(CONNECTION_TIMEOUT_IN_SECS * 1000);// 10 secs
+					hikariConfig.setIdleTimeout(IDLE_TIMEOUT_SECS * 1000);
+					hikariConfig.setMaxLifetime(MAX_LIFE_TIME_IN_MINS * 60 * 1000);// 30 MINS 30*60*1000
+					hikariConfig.setPoolName(HIKARI_CP_POOL_NAME);
+					hikariDataSource = new HikariDataSource(hikariConfig);
 				}
-
 			}
+
 		}
+
 	}
-	public static void main(String[] args) throws SQLException {
-		DataBaseManager.createConnection();
-		DataBaseManager.createConnection();
-		DataBaseManager.createConnection();
-		
+
+	public static Connection getConnection() throws SQLException {
+		Connection connection = null;
+		if (hikariDataSource == null) {
+			intializePool(); // Automatic Intialization of HikariDataSource
+		}
+
+		else if (hikariDataSource.isClosed()) {
+			throw new SQLException("HIKARI DATA SOURCE IS CLOSED");
+		}
+
+		connection = hikariDataSource.getConnection();
+
+		return connection;
+
 	}
 }
-
-	
-
--- src/test/java/com/database/JDBCDemmo.java
+++ src/test/java/com/database/JDBCDemmo.java
@@ -10,8 +10,8 @@ public class JDBCDemmo {
 		// TODO Auto-generated method stub
 
 //		Connection connection = DriverManager.getConnection("jdbc:mysql://64.227.160.186 :3306", "srdev_ro_automation","Srdev@123");
-		DataBaseManager.createConnection();
-
+ Connection conn = DataBaseManager.getConnection();
+System.out.println(conn);
 	
 	}}
 
